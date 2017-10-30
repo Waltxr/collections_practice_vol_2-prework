0edc40da1ffbac462bc7cc3895532f1791772140
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-30 17:49:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.each { |tool| tool[0] == "r" }
+end
+
+def contain_a(array)
+
+end
+
+def first_wa(array)
+
+end
+
+def remove_non_strings(array)
+
+end
+
+def count_elements(array)
+
+end
+
+def merge_data(data1, data2)
+
+end
+
+def find_cool(hashes)
+
+end
+
+def organize_schools(schools)
+
+end
